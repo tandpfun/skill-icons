4391cd40c3d48416644277fef4b9f6cede0ba216
tandpfun <dev.tandpfun@gmail.com>
2022-03-02 20:01:56 -0800

    miniflare, improvements, better kt logos

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
 const icons = require("./icons.json");
-
-const ICONS_PER_LINE = 15;
-const SVG_WIDTH = 800;
-
-function generateSvg(iconNames) {
-	const iconSvgList = iconNames.map((i) => icons[i]);
-
-	const length = ICONS_PER_LINE * 300;
-	const height = Math.ceil(iconSvgList.length / ICONS_PER_LINE) * 300 - 44;
-	const scaledHeight = height * (SVG_WIDTH / length);
-
-	return `
-  <svg width="${SVG_WIDTH}" height="${scaledHeight}" viewBox="0 0 ${length} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
-    ${iconSvgList
-			.map(
-				(i, index) =>
-					`
-        <g transform="translate(${(index % ICONS_PER_LINE) * 300}, ${
-						Math.floor(index / ICONS_PER_LINE) * 300
-					})">
-          ${i}
-        </g>
-        `
-			)
-			.join(" ")}
-  </svg>
-  `;
-}
-
 const longNames = {
 	js: "javascript",
 	ts: "typescript",
@@ -52,7 +23,7 @@ const longNames = {
 	cpp: "c++",
 	c: "c",
 	swift: "swift",
-	kt: "kotlin",
+	kotlin: "kotlin",
 	dart: "dart",
 	wasm: "webassembly",
 	mysql: "mysql",
@@ -88,7 +59,35 @@ const themedIcons = [
 	"vim",
 ];
 
-function parseShortNames(names, theme = "light") {
+const ICONS_PER_LINE = 15;
+const SVG_WIDTH = 800;
+
+function generateSvg(iconNames) {
+	const iconSvgList = iconNames.map((i) => icons[i]);
+
+	const length = ICONS_PER_LINE * 300;
+	const height = Math.ceil(iconSvgList.length / ICONS_PER_LINE) * 300 - 44;
+	const scaledHeight = height * (SVG_WIDTH / length);
+
+	return `
+  <svg width="${SVG_WIDTH}" height="${scaledHeight}" viewBox="0 0 ${length} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+    ${iconSvgList
+			.map(
+				(i, index) =>
+					`
+        <g transform="translate(${(index % ICONS_PER_LINE) * 300}, ${
+						Math.floor(index / ICONS_PER_LINE) * 300
+					})">
+          ${i}
+        </g>
+        `
+			)
+			.join(" ")}
+  </svg>
+  `;
+}
+
+function parseShortNames(names, theme = "dark") {
 	return names.map(
 		(name) =>
 			longNames[name] +
@@ -100,16 +99,21 @@ async function handleRequest(request) {
 	const { pathname, searchParams } = new URL(request.url);
 
 	const path = pathname.split("/")[1];
-	if (path === "icons") {
+	if (path === "icons" || path === "icons.svg") {
 		const iconParam = searchParams.get("i");
 		if (!iconParam) return new Response("No icons specified", { status: 400 });
+		const theme = searchParams.get("t");
+		if (theme && theme !== "dark" && theme !== "light")
+			return new Response("Invalid theme param", { status: 400 });
 		const iconShortNames = iconParam.split(",");
-		const iconNames = parseShortNames(iconShortNames);
+		const iconNames = parseShortNames(iconShortNames, theme || undefined);
 		if (!iconNames) return new Response("Invalid icon params", { status: 400 });
 
 		const svg = generateSvg(iconNames);
 
 		return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } });
+	} else {
+		return new Response("404: Not Found", { status: 404 });
 	}
 }
 
