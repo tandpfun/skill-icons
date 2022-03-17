0a2b5d2ad1d66ab6b047f8c80a471d60ef9389c5
ThatOneCalculator <kainoa@t1c.dev>
2022-03-16 23:07:55 -0700

    gaming

--- index.js
+++ index.js
@@ -35,6 +35,121 @@ const shortNames = {
   nest: 'nestjs',
   ktorio: 'ktor',
 };
+const urlMaps = {
+  aws: 'https://aws.amazon.com/',
+  activitypub: 'https://activitypub.rocks/',
+  actix: 'https://actix.rs/',
+  aftereffects: 'https://www.adobe.com/products/aftereffects.html',
+  aiscript: 'https://github.com/syuilo/aiscript',
+  angular: 'https://angular.io/',
+  ansible: 'https://www.ansible.com/',
+  apollo: 'https://www.apollographql.com/',
+  appwrite: 'https://appwrite.io/',
+  autocad: 'https://www.autodesk.com/products/autocad/overview',
+  azul: 'https://azul.rs/',
+  bsd: 'https://bsd.org/',
+  bash: 'https://www.gnu.org/software/bash/',
+  blender: 'https://www.blender.org/',
+  bootstrap: 'https://getbootstrap.com/',
+  c: 'https://www.iso.org/standard/74528.html',
+  cpp: 'https://isocpp.org/',
+  cs: 'https://docs.microsoft.com/en-us/dotnet/csharp/',
+  css: 'https://www.w3.org/TR/CSS/#css',
+  clojure: 'https://clojure.org/',
+  cloudflare: 'https://www.cloudflare.com/',
+  coffeescript: 'https://coffeescript.org/',
+  deno: 'https://deno.land/',
+  dart: 'https://dart.dev/',
+  discord: 'https://discord.com/',
+  discordbots: 'https://discord.com/developers/docs/intro',
+  docker: 'https://www.docker.com/',
+  dotnet: 'https://dotnet.microsoft.com/',
+  eclipse: 'https://www.eclipse.org/',
+  electron: 'https://www.electronjs.org/',
+  emacs: 'https://www.gnu.org/software/emacs/',
+  emotion: 'https://emotion.sh/',
+  express: 'https://expressjs.com/',
+  figma: 'https://www.figma.com/',
+  firebase: 'https://firebase.google.com/',
+  flutter: 'https://flutter.dev/',
+  fortran: 'https://fortran-lang.org/',
+  gcp: 'https://cloud.google.com/',
+  gtk: 'https://www.gtk.org/',
+  gamemakerstudio: 'https://www.yoyogames.com/en/gamemaker',
+  gatsby: 'https://www.gatsbyjs.com/',
+  gherkin: 'https://cucumber.io/docs/gherkin/',
+  git: 'https://git-scm.com/',
+  golang: 'https://go.dev/',
+  godot: 'https://godotengine.org/',
+  grafana: 'https://grafana.com/',
+  graphql: 'https://graphql.org/',
+  html: 'https://html.spec.whatwg.org/',
+  haxe: 'https://haxe.org/',
+  haxeflixel: 'https://haxeflixel.com/',
+  idea: 'https://www.jetbrains.com/idea/',
+  illustrator: 'https://www.adobe.com/products/illustrator.html',
+  jquery: 'https://jquery.com/',
+  java: 'https://java.com/',
+  javascript: 'https://www.javascript.com/',
+  jenkins: 'https://www.jenkins.io/',
+  kotlin: 'https://kotlinlang.org/',
+  ktor: 'https://ktor.io/',
+  kubernetes: 'https://kubernetes.io/',
+  latex: 'https://www.latex-project.org/',
+  laravel: 'https://laravel.com/',
+  linux: 'https://kernel.org/',
+  lua: 'https://www.lua.org/',
+  markdown: 'https://daringfireball.net/projects/markdown/',
+  materialui: 'https://mui.com/',
+  mongodb: 'https://www.mongodb.com/',
+  mysql: 'https://www.mysql.com/',
+  nestjs: 'https://nestjs.com/',
+  nextjs: 'https://nextjs.org/',
+  nim: 'https://nim-lang.org/',
+  nodejs: 'https://nodejs.org/',
+  nuxtjs: 'https://nuxtjs.org/',
+  openshift: 'https://www.redhat.com/en/technologies/cloud-computing/openshift',
+  php: 'https://www.php.net/',
+  photoshop: 'https://www.adobe.com/products/photoshop.html',
+  plan9: 'https://9p.io/plan9/',
+  postgresql: 'https://www.postgresql.org/',
+  premiere: 'https://www.adobe.com/products/premiere.html',
+  prisma: 'https://www.prisma.io/',
+  prometheus: 'https://prometheus.io/',
+  pug: 'https://pugjs.org/',
+  python: 'https://www.python.org/',
+  qt: 'https://www.qt.io/',
+  r: 'https://www.r-project.org/',
+  rails: 'https://rubyonrails.org/',
+  react: 'https://reactjs.org/',
+  redis: 'https://redis.io/',
+  remix: 'https://remix.run/',
+  rust: 'https://www.rust-lang.org/',
+  rocket: 'https://rocket.rs/',
+  svg: 'https://www.w3.org/Graphics/SVG/',
+  sass: 'https://sass-lang.com/',
+  scala: 'https://www.scala-lang.org/',
+  solidity: 'https://soliditylang.org/',
+  spring: 'https://spring.io/',
+  stackoverflow: 'https://stackoverflow.com/',
+  styledcomponents: 'https://styled-components.com/',
+  supabase: 'https://supabase.com/',
+  tailwindcss: 'https://tailwindcss.com/',
+  tauri: 'https://tauri.studio/',
+  typescript: 'https://www.typescriptlang.org/',
+  unity: 'https://unity.com/',
+  unrealengine: 'https://www.unrealengine.com/',
+  v: 'https://vlang.io/',
+  vim: 'https://www.vim.org/',
+  vscode: 'https://code.visualstudio.com/',
+  vala: 'https://wiki.gnome.org/Projects/Vala',
+  vuejs: 'https://vuejs.org/',
+  webassembly: 'https://webassembly.org/',
+  windicss: 'https://windicss.org/',
+  wordpress: 'https://wordpress.org/',
+  workers: 'https://workers.cloudflare.com/',
+  zig: 'https://ziglang.org/',
+};
 const themedIcons = [
   ...Object.keys(icons)
     .filter(i => i.includes('-light') || i.includes('-dark'))
@@ -44,6 +159,7 @@ const themedIcons = [
 const ICONS_PER_LINE = 15;
 const ONE_ICON = 48;
 const SCALE = ONE_ICON / (300 - 44);
+const CLICKABLE = false;
 
 function generateSvg(iconNames, perLine) {
   const iconSvgList = iconNames.map(i => icons[i]);
@@ -59,8 +175,7 @@ function generateSvg(iconNames, perLine) {
       .map(
         (i, index) =>
           `
-        <g transform="translate(${(index % perLine) * 300}, ${
-            Math.floor(index / perLine) * 300
+        <g transform="translate(${(index % perLine) * 300}, ${Math.floor(index / perLine) * 300
           })">
           ${i}
         </g>
@@ -69,6 +184,14 @@ function generateSvg(iconNames, perLine) {
       .join(' ')}
   </svg>
   `;
+}
+
+function generateText(iconNames) {
+  let renderedText = "";
+  iconNames.split(",").forEach(element => {
+    renderedText += `<a href="${urlMaps[element]}"><img src="https://skillicons.dev/icons?i=${element}" /></a> `;
+  });
+  return renderedText;
 }
 
 function parseShortNames(names, theme = 'dark') {
@@ -113,9 +236,19 @@ async function handleRequest(request) {
         status: 400,
       });
 
-    const svg = generateSvg(iconNames, perLine);
-
-    return new Response(svg, { headers: { 'Content-Type': 'image/svg+xml' } });
+    const clickable = searchParams.get('clickable') || CLICKABLE;
+    if (clickable) {
+      const text = generateText(iconsNames);
+      return new Response(JSON.stringify({ text }), {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+    }
+    else {
+      const svg = generateSvg(iconNames, perLine);
+      return new Response(svg, { headers: { 'Content-Type': 'image/svg+xml' } });
+    }
   } else if (path === 'api/icons') {
     return new Response(JSON.stringify(iconNameList), {
       headers: {
